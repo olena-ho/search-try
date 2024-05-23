1269fa571f147bcf463753fccaf293b12a8c7ad0
Viktoriia Nykyforova <113258083+ViktoriiaKN@users.noreply.github.com>
2024-05-23 15:36:46 +0200

    add hotels.json

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,12 +1,14 @@
 import { SearchBar } from '../../components/SearchBar';
 import Map from '../../components/Map';
+import hotels from '../../data/hotels.json';
 import './style.css';
 
 
 export const HomePage = () => {
   return (
 
-    <><SearchBar />
+    <>
+    <SearchBar />
    <div className="home-page">
     <div className="left-content">
     </div>
